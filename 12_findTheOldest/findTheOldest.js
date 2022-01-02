610c3cf622f63c17127057e5aef908fdf782d7fc
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-02 17:39:33 -0600

    finish with fin the oldest project

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    let sorted =  array.sort(function(item1, item2) {
+        let age1 = 0
+        let age2 = 0
+        if (item1.hasOwnProperty('yearOfDeath')){
+             age1 = item1.yearOfDeath - item1.yearOfBirth;
+        }
+        else {
+            age1 = new Date().getFullYear - item1.yearOfBirth;
+        }
+        if (item2.hasOwnProperty('yearOfDeath')) {
+         age2 = item2.yearOfDeath - item2.yearOfBirth;
+        }
+        else {
+         age2 = new Date().getFullYear() - item2.yearOfBirth;
+        }
+         if (age1 > age2) {
+             return 1;
+         }
+         else {
+             return -1;
+         }
+     })
+     return sorted.at(-1);
+ };
 
 // Do not edit below this line
 module.exports = findTheOldest;
