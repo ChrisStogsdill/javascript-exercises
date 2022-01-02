4dd192c85287bd94a9db9d9a53a667357311dc86
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-02 13:17:25 -0600

    finish calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(function(a, b) {
+    return a +b;
+  }, 0)
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+	return array.reduce(function(a, b) {
+    return a * b;
+  }, 1)
 };
 
-const power = function() {
-	
-};
+const power = function(a, b) {
+  return Math.pow(a, b)
+  };
 
-const factorial = function() {
-	
-};
+  const factorial = function(number) {
+    let total = 1;
+    if (number == 0) {return 1; }
+    else {
+      for (i = number; i > 0; i--) {
+        total *= i;
+      }
+      return total;
+    }
+  };
 
 // Do not edit below this line
 module.exports = {
