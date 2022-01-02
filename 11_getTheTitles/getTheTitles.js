5fa03dcb156d7f9cb20a12d395b944cb9a4617fd
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-02 16:18:19 -0600

    finish get titles project

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    output = [];
+array.map(function(entry) {
+    output.push(entry.title);
+})
+return output;
 };
 
 // Do not edit below this line
